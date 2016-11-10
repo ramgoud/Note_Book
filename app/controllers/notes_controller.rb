09efe59c712de086154afccce2b04d226d44a3d3
RAMU <ramugoud530@gmail.com>
2016-11-10 14:53:43 -0600

    Installed Devise

--- app/controllers/notes_controller.rb
+++ app/controllers/notes_controller.rb
@@ -1,34 +1,42 @@
 class NotesController < ApplicationController
 	before_action :find_note, only: [:show, :edit, :update, :destroy]
-	
+	before_action :authenticate_user!
+
 	def index
-		@notes = Note.all.order("created_at DESC")
+		@notes = Note.where(user_id: current_user)
 	end
 
 	def show
 	end
 
 	def new
-		@note = Note.new
+		@note = current_user.notes.build
 	end
 
 	def create
-		@note = Note.new(note_params)
+		@note = current_user.notes.build(note_params)
 
 		if @note.save
 			redirect_to @note
 		else
 			render 'new'
-		end 
-	end 
+		end
+	end
 
 	def edit
 	end
 
 	def update
+		if @note.update(note_params)
+			redirect_to @note
+		else
+			render 'edit'
+		end
 	end
 
 	def destroy
+		@note.destroy
+		redirect_to notes_path
 	end
 
 	private
@@ -38,6 +46,6 @@ class NotesController < ApplicationController
 	end
 
 	def note_params
-		params.require(:note).permit(:title , :content)
-	end 
+		params.require(:note).permit(:title, :content)
+	end
 end
